ae5155045e9e385c0e49c01d29258afc1f66b44a
Julien Kunze <julienkunze0@gmail.com>
2023-04-12 17:13:32 +0200

    Customize easyadmin dashboard and adding crud controller

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Horaire;
+use App\Entity\ImagePlats;
+use App\Controller\Admin\ImagePlatsCrudController;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator)
+    {
+        
+    }
+       
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        $url = $this->adminUrlGenerator
+         ->setController(ImagePlatsCrudController::class)
+         ->generateUrl();
+         return $this->redirect($url);
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Quai Antic');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+    
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'app_home');
+        yield MenuItem::linkToCrud('Images des plats', 'fas fa-map-marker-alt', ImagePlats::class);
+        yield MenuItem::linkToCrud('Horaires', 'fas fa-comments', Horaire::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+    }
+}
