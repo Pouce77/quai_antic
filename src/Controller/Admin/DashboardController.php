4d6764fddb1e7c5ae6cd5e13d79237c21a24f85e
Julien Kunze <julienkunze0@gmail.com>
2023-07-03 18:11:34 +0200

    adding form end

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -29,22 +29,11 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
+        
         $url = $this->adminUrlGenerator
          ->setController(ReservationCrudController::class)
          ->generateUrl();
          return $this->redirect($url);
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
